0838fe69228d1e7ec8be000019cd36219d94df22
yonezawa keita <e195731@ie.u-ryukyu.ac.jp>
2019-12-02 18:34:56 +0900

    second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("例外起きた");
+            System.out.println(e.getMessage());
+        }
     }
 }
